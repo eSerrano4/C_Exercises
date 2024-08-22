3c977193b670f165c966cdd1b12de4a41aac9350
eserrano4 <beatjunkieeric@gmail.com>
2024-08-22 10:33:41 -0700

    Diagonal triangle exercise

--- StudentExercises.cpp
+++ StudentExercises.cpp
@@ -239,7 +239,7 @@ int main() {
 }*/
 
 //create star square pattern using nested loop
-#include<iostream>
+/*#include<iostream>
 
 using namespace std;
 
@@ -250,5 +250,20 @@ int main() {
 			cout << "* ";
 		}
 		cout << endl;
+	}
+}*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	for(int i = 0; i<4; i++){
+		for(int j = 0; j<=4; j++ ){
+			if(i>=j){
+				cout<<"* ";
+			}
+		}
+		cout<<endl;
+
 	}
 }
