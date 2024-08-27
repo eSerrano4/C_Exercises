f7a709b5ba980b9d10f0a11010f7b6e7f8b321e5
eserrano4 <beatjunkieeric@gmail.com>
2024-08-27 14:39:16 -0700

    Calculate avg of items in array exercise

--- StudentExercises.cpp
+++ StudentExercises.cpp
@@ -285,5 +285,41 @@ for(int i = 0;i<n;i++){
 	}cout<<endl;
 }
 }*/
+//multiple 2d array
+/*#include<iostream>
+using namespace std;
 
+int main(){
+	int a[2][3]={{1,2,3},{4,5,6}};
+	int b[2][3]={{7,8,9},{3,5,7}};
+	int c[2][3];
 
+	for(int i = 0; i<2;i++){
+		for(int j =0; j<3;j++){
+			c[i][j] = a[i][j]*b[i][j];
+			cout<<c[i][j]<<" ";
+		}
+	}cout<<endl;
+}*/
+
+//Calculate average of all elements in an array 
+
+/*#include<iostream>
+using namespace std;
+
+int main(){
+	int n, sum =0;
+	cout<<"Please enter number: ";
+	cin>>n;
+
+	int arr[n];
+	int avg;
+
+	for(int i = 0; i<n; i++){
+		arr[i]= rand() % 100;
+		cout<<arr[i]<<endl;
+		sum += arr[i];
+	}
+
+	cout<<"Answer: "<<(avg = sum/n)<<endl;
+}*/
